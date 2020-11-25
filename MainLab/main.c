a326cc6c940237ce7bf29589367bb2d54dcfc904
Biarki Weeks <biarkiw@gmail.com>
2020-11-25 15:02:07 -0800

    testing

--- MainLab/main.c
+++ MainLab/main.c
@@ -5,7 +5,8 @@
 #include "cmsis/cmsis_device.h"
 
 /*
-	To Do
+
+To Do
 
 1.Enable and configure ADC
 2.Enable and configure GPIOC
@@ -19,7 +20,7 @@
 // Select Channel 11 and Convert it with 239.5 Cycles as sampling time
 // Enable the ADC peripheral
 // Wait the ADRDY flag
-
+*/
 // ----- main() ---------------------------------------------------------------
 
 // Sample pragmas to cope with warnings. Please note the related line at
@@ -39,8 +40,9 @@
 
 //Port and peripheral initialization
 void myClock_Init(){
+
   //enable clock for ports A,B & C
-  RCC->AHBENR= 0x000E0000;
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN;
   //trace_printf("%x \n",RCC->AHBENR);
 
   //enable clock for ADC & DAC
@@ -48,33 +50,26 @@ void myClock_Init(){
   //trace_printf("%x \n",RCC->APB2ENR);
   RCC->APB1ENR=RCC_APB1ENR_DACEN;
   //trace_printf("%x \n",RCC->APB1ENR);
-}
-
-void myCOM_Init(){
 
 }
 
 void myGPIOA_Init(){
-	/* Enable clock for GPIOA peripheral */
-	// Relevant register: RCC->AHBENR
-	//RCC->AHBENR = 0x0200;
 
-	/* Configure PA2 as input */
-	// Relevant register: GPIOA->MODER
-	//GPIOA->MODER= 0x0;
+	/* Configure PA1 as input & PA4 as analog for DAC */
+	GPIOA->MODER= 0x00000300;
 
-	/* Ensure no pull-up/pull-down for PA2 */
-	// Relevant register: GPIOA->PUPDR
-	//GPIOA->PUPDR = 0x0; //potetnial change to 0x3 if not acting as intetnded
+	/* Ensure no pull-up/pull-down for PA */
+	GPIOA->PUPDR = 0x0;
+
 }
 
 void myGPIOB_Init(){
+
 	// Configure PB as output
 	GPIOB->MODER = 0x55555555;
 	//ensure no pull up, no pull down
 	GPIOB->PUPDR = 0x0;
 
-	//GPIOB->ODR =
 }
 
 void myGPIOC_Init(){
@@ -82,28 +77,11 @@ void myGPIOC_Init(){
   //Configure PC1 as analog input
   GPIOC->MODER = 0x03;
 
-  //
-
 }
 
 void myADC_Init() {
 
-	/*//calibrate ADC
-	ADC_CR_ADCAL;
-	trace_printf("Calibrating ADC \n");
-
-	//Wait for calibration to be completed
-	while(ADC_CR_ADCAL != 0){
-		trace_printf("calibrating... \n");
-	}
-
-  //print calibration factor
-  int cal = ADC_DR_DATA;
-  trace_printf("Calibration factor of: %u", cal);
-
-  //set clock mode
-  ADC_CFGR2_CKMODE_0;
-*/
+  /* calibrate ADC */
   if((ADC1->CR & ADC_CR_ADEN)!= 0){
     //if adc is enabled, disable adc
     ADC1->CR |= ADC_CR_ADDIS;
@@ -117,22 +95,42 @@ void myADC_Init() {
     /* wait for calibration to finish */
   }
   int cal = (ADC1->DR) & 0x3F;
-  trace_printf("Calibration factor of: %u", cal);
+  trace_printf("Calibration factor of: %u\n", cal);
+
+  /* enable ADC */
+  if ((ADC1->ISR & ADC_ISR_ADRDY) != 0)  {
+    ADC1->ISR |= ADC_ISR_ADRDY;
+  }
+  ADC1->CR |= ADC_CR_ADEN;
+  while ((ADC1->ISR & ADC_ISR_ADRDY) == 0)  {
+    /* wait for ADC to be enabled */
+  }
+
+  // Configure the ADC1 in continuous mode, Overrun mode, right data alignment with a resolution equal to 12 bits
+  ADC1->CFGR1 |= (ADC_CFGR1_CONT | ADC_CFGR1_OVRMOD) & ((~ADC_CFGR1_ALIGN) & (~ADC_CFGR1_RES));
 
   // Configure ADC Channel11 as analog input
-  ADC->CHSELR |=
-  // Configure the ADC1 in continuous mode, Overrun mode, right data alignment with a resolution equal to 12 bits
+  ADC1->CHSELR |= ADC_CHSELR_CHSEL11;
+
   // Select Channel 11 and Convert it with 239.5 Cycles as sampling time
-  // Enable the ADC peripheral
-  // Wait the ADRDY flag
+  ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2;
+  trace_printf("ADC calibrated and configured, starting continuous conversion \n");
+
+  // start the ADC converting
+  ADC1->CR |= ADC_CR_ADSTART;
+
 }
 
+void myDAC_Init(){
+
+  //
+}
 
 //Interrupt Handlers
 
 
 //Gobal variables
-
+int potVal = 0;
 
 
 int main(int argc, char* argv[]){
@@ -148,6 +146,11 @@ int main(int argc, char* argv[]){
 	while (1){
 
 
+    if((ADC1->ISR & ADC_ISR_EOC) != 0 ){
+      ADC1->ISR &= ~ADC_ISR_EOC;
+      potVal = ADC1->DR & 0xFFFF;
+      trace_printf("pot val : %u\n", potVal);
+    }
 	}
 
 	return 0;
