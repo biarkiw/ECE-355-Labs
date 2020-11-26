f50c006bf6b3baecc22575ff8ac9580081e60d75
Biarki Weeks <biarkiw@gmail.com>
2020-11-25 17:10:10 -0800

    test3

--- MainLab/main.c
+++ MainLab/main.c
@@ -32,10 +32,14 @@ To Do
 
 
 /* Clock prescaler for TIM2 timer: no prescaling */
-//#define myTIM2_PRESCALER ((uint16_t)0x0000)
+#define myTIM2_PRESCALER ((uint16_t)0x0000)
 /* Maximum possible setting for overflow */
-//#define myTIM2_PERIOD ((uint32_t)0xFFFFFFFF)
+#define myTIM2_PERIOD ((uint32_t)0xFFFFFFFF)
 
+//Gobal variables
+int potVal = 0;
+int resVal = 0;
+unsigned char timTrig = 0;
 
 
 //Port and peripheral initialization
@@ -45,10 +49,12 @@ void myClock_Init(){
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN;
   //trace_printf("%x \n",RCC->AHBENR);
 
-  //enable clock for ADC & DAC
-  RCC->APB2ENR=RCC_APB2ENR_ADCEN;
+  //enable clock for ADC
+  RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
   //trace_printf("%x \n",RCC->APB2ENR);
-  RCC->APB1ENR=RCC_APB1ENR_DACEN;
+
+  //enable clock for DAC & TIM2
+  RCC->APB1ENR |= RCC_APB1ENR_DACEN | RCC_APB1ENR_TIM2EN;
   //trace_printf("%x \n",RCC->APB1ENR);
 
 }
@@ -128,28 +134,152 @@ void myDAC_Init(){
 
 }
 
+void myTIM2_Init(){
+
+	/* Configure TIM2: buffer auto-reload, count up, stop on overflow,
+	 * enable update events, interrupt on overflow only */
+	TIM2->CR1 = ((uint16_t)0x008C);
+
+	/* Set clock prescaler value */
+	TIM2->PSC = myTIM2_PRESCALER;
+
+	/* Set auto-reloaded delay */
+	TIM2->ARR = myTIM2_PERIOD;
+
+	/* Update timer registers */
+	// Relevant register: TIM2->EGR
+	TIM2->EGR = 0x01;
+
+	/* Assign TIM2 interrupt priority = 0 in NVIC */
+	NVIC_SetPriority(TIM2_IRQn, 0);
+
+	/* Enable TIM2 interrupts in NVIC */
+	NVIC_EnableIRQ(TIM2_IRQn);
+
+	/* Enable update interrupt generation */
+	TIM2->DIER = 0x1;
+}
+
+void myEXTI_Init(){
+	/* Map EXTI2 line to PA1 */
+	// Relevant register: SYSCFG->EXTICR[0]
+	SYSCFG->EXTICR1  = SYSCFG_EXTICR1_EXTI1_PA;
+
+	/* EXTI1 line interrupts: set rising-edge trigger */
+	// Relevant register: EXTI->RTSR
+	EXTI->RTSR=0x2;
+
+	/* Unmask interrupts from EXTI1 line */
+	// Relevant register: EXTI->IMR
+	EXTI->IMR= 0x2;
+
+	/* Assign EXTI1 interrupt priority = 0 in NVIC */
+	// Relevant register: NVIC->IP[2], or use NVIC_SetPriority
+	//NVIC->IP[2] = 0x0;
+	NVIC_SetPriority(EXTI0_1_IRQn, 0);
+
+	/* Enable EXTI2 interrupts in NVIC */
+	// Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
+	NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
+
+
 //Interrupt Handlers
 
+void TIM2_IRQHandler(){
+	/* Check if update interrupt flag is indeed set */
+	if ((TIM2->SR & TIM_SR_UIF) != 0)
+	{
+		trace_printf("\n*** Overflow! ***\n");
 
-//Gobal variables
-int potVal = 0;
-int resVal = 0;
+		/* Clear update interrupt flag */
+		// Relevant register: TIM2->SR
+		TIM1->SR &= ~(TIM_SR_UIF);
+
+		/* Restart stopped timer */
+		// Relevant register: TIM2->CR1
+		TIM2->CR1 |= TIM_CR1_CEN;
+
+	}
+}
+
+void EXTI0_1_IRQHandler(){
+	// Declare/initialize your local variables here...
+	unsigned int time = 0;
+	unsigned int period = 0;
+
+	/* Check if EXTI1 interrupt pending flag is indeed set */
+	if ((EXTI->PR & EXTI_PR_PR1) != 0){
+		//
+		// 1. If this is the first edge:
+
+		//
+		// 2. Clear EXTI2 interrupt pending flag (EXTI->PR).
+		// NOTE: A pending register (PR) bit is cleared
+		// by writing 1 to it.
+		//
+		if(timTrig == 0){
+			//set flag that timer has been trigger
+			timTrig = 1;
+
+			//	- Clear count register (TIM2->CNT).
+			TIM2->CNT =0X00000000;
+
+			//	- Start timer (TIM2->CR1).
+			TIM2->CR1 |= TIM_CR1_CEN;
+
+			//    Else (this is the second edge):
+		}else if (timTrig == 1){
+
+
+
+			//	- Stop timer (TIM2->CR1).
+			TIM2->CR1 ^= TIM_CR1_CEN;
+			//	- Read out count register (TIM2->CNT).
+			time = TIM2->CNT;
+			//	- Calculate signal period and frequency.
+			time = SystemCoreClock/time;
+			period = (1000000/time);
+
+			//	- Print calculated values to the console.
+			trace_printf("Frequency: %u Hz \n",time);
+			trace_printf("Period: %u microseconds \n", period);
+
+			//	  NOTE: Function trace_printf does not work
+			//	  with floating-point numbers: you must use
+			//	  "unsigned int" type to print your signal
+			//	  period and frequency.
+
+			//set flag that timer has been read
+			timTrig = 0;
+		}
+		//
+		// 2. Clear EXTI1 interrupt pending flag (EXTI->PR).
+		// NOTE: A pending register (PR) bit is cleared
+		// by writing 1 to it.
+		//
+		EXTI->PR = 0xFFFF;
+	}
+}
+
 
 int main(int argc, char* argv[]){
 
 	trace_printf("System clock: %u Hz\n", SystemCoreClock);
 
 	myClock_Init();    /* Initialize peripheral clocks */
-	myADC_Init();      /* Initialize ADC */
 	myGPIOA_Init();    /* Initialize I/O port A */
 	myGPIOB_Init();    /* Initialize I/O port B */
   myGPIOC_Init();    /* Initialize I/O port C */
+  myADC_Init();      /* Initialize ADC */
+  myDAC_Init();      /* Initialize DAC */
 
 	while (1){
 
     if((ADC1->ISR & ADC_ISR_EOC) != 0 ){
       ADC1->ISR &= ~ADC_ISR_EOC;
       potVal = ADC1->DR & 0xFFFF;
+      DAC->DHR12R1 = potVal & 0xFFF;
       resVal = 5000*(potVal/4020);
       // 5kohm = 4020ish
       trace_printf("pot val : %u\n", resVal);
